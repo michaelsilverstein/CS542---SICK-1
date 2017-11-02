e4e0add526a98fc8d9f8c4d591023a2ce0236462
Long Nguyen <long.vnguyenn@gmail.com>
2017-11-02 14:25:33 -0400

    download data from google drive

---
+++ download_data.py
@@ -0,0 +1,16 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# install PyDrive : pip install PyDrive
+
+# need to store client_secrets in the same working directory
+
+gauth = GoogleAuth()
+gauth.LocalWebServerAuth()
+
+drive = GoogleDrive(gauth)
+# search for files with titles contain 'production_xmldata*'
+for file_list in drive.ListFile({'q': 'title contains "production_xmldata"'}):
+    for file in file_list:
+        #download file - keep its original name
+	file.GetContentFile(file['title'])
